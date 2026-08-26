ec66293883fc959933671138eb2c5d6e6ccb131e
Debasish Roy <dkroy3300@gmail.com>
2026-08-26 23:29:29 +0600

    Install laravel for new project

---
+++ resources/views/admin/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="adminHMD professional admin dashboard template">
+  <title>Dashboard | adminHMD</title>
+    @include('admin.components.css')
+</head>
+
+<body>
+  <div class="admin-shell">
+    <div class="sidebar-backdrop" data-sidebar-close></div>
+      @include('admin.components.sidebar')
+    <div class="admin-main">
+        @include('admin.components.nav')
+
+
+      @include('admin.components.footer')
+    </div>
+  </div>
+
+@include('admin.components.js')
+</body>
+</html>
---
+++ resources/views/admin/components/css.blade.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="{{ ('assets/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ ('assets/vendors/bootstrap-icons/bootstrap-icons.css') }}">
+<link rel="stylesheet" href="{{ ('assets/css/style.css') }}">
